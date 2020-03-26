e474a67bfec24983841e7f63ae6ffcb7585e39d2
rainstr7 <rainstr7@gmail.com>
2020-03-26 22:11:35 +0300

    config test

--- src/magic.js
+++ src/magic.js
@@ -1,6 +1,7 @@
-export default (...numbers) => {
+ const magic = (...numbers) => {
   const sum = numbers.reduce((acc, num) => num + acc, 0);
   const inner = (...rest) => magic(sum, ...rest);
   inner.valueOf = () => sum;
   return inner;
 };
+export default magic;
