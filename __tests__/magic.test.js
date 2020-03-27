1472d33f5242a4fad4ebd6d2d88d78ecd2f70ff3
rainstr7 <rainstr7@gmail.com>
2020-03-27 14:00:21 +0300

    correct test

--- __tests__/magic.test.js
+++ __tests__/magic.test.js
@@ -14,7 +14,6 @@ describe('magic', () => {
     magic(4, 5);
     expect(magic(5, 2, -8) + 2).toBe(1);
     expect(magic(1, 2)(3, 4, 5)(6)(7, 10) - 8).toBe(30);
-    expect(magic(4, 8, 1, -1, -8)(3)(-3)(7, 2) + 7).toBe(20);
     magic(1, 3, 4);
     expect(magic(5) + 1).toBe(6);
   });
